f929b1c779aa7c628c9280e14118bac9f68929a8
MaciejJaromin <maciej_jaromin@o2.pl>
2014-10-26 22:41:23 +0100

    Validations fixed.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  validate :name, presence: true
-  validate :price, numericality: {greater_than: 0}
-  validate :category, presence: true
+  validates :name, presence: true
+  validates :price, numericality: {greater_than: 0}
+  validates_presence_of :category
 end
